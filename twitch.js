cd2bb7e57851f4778b0da76aa9f1740f8b6ab5c2
= <krillinmaster@gmail.com>
2017-01-16 14:35:27 -0800

    Added JavaScript function for getting the info of live channels

--- twitch.js
+++ twitch.js
@@ -12,24 +12,46 @@
 
 
  //IE 10+
+ let twitchList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+     "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"
+ ]
+ let len = twitchList.length
+ for (var i = 0; i < len; i++) {
+     let request = new XMLHttpRequest();
+     let url = 'https://wind-bow.gomix.me/twitch-api/streams/' + twitchList[i] + "?"
+     request.open('GET', url, true);
 
- let request = new XMLHttpRequest();
- let url = 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?'
- request.open('GET', url, true);
+     request.onload = function() {
+         if (this.status >= 200 && this.status < 400) {
+             // success codes
+             let data = JSON.parse(this.response);
+             console.log(data);
+             if (data.stream) {
+                 // if the stream is online
+                 let container = document.getElementById("container")
+                     //          <div class="user-wrapper">
+                     //     <img src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png" class="user-image">
+                     //     <p class="user-name">User.Name</p>
+                     //     <span class="indicator-on"></span>
+                     //     <p class="title">User.Title</p>
+                     // </div>
 
- request.onload = function() {
-     if (this.status >= 200 && this.status < 400) {
-         // success codes
-         let data = JSON.parse(this.response);
-         console.log(data._links.channel);
-     } else {
-         // returned an error
-         alert("Error");
-     }
- };
+                 let appendString = "<div class='user-wrapper'>" +
+                     "<img src='" + data.stream.channel.logo + "' class = 'user-image'>" +
+                     "<p class='user-name'>" + twitchList[i] + "</p>" +
+                     "<span class='indicator-on'></span>" +
+                     "<p class='title'>" + data.stream.channel.game + "</p></div>";
+                 container.innerHTML += appendString;
+             }
+         } else {
+             // returned an error
+             alert("Error");
+         }
+     };
 
- request.onerror = function() {
-     alert("Actual connection error");
- };
+     request.onerror = function() {
+         alert("Actual connection error");
+     };
 
- request.send();
+     request.send();
+ }
