6e65edd5cb5e2c30f47d9a39bfb82b4091783a07
b0ssin <krillinmaster@gmail.com>
2017-01-28 19:25:24 -0500

    Fixed kinks in event listeners

--- listener.js
+++ listener.js
@@ -1,4 +1,58 @@
 $(document).ready(function() {
+    
+    /** AJAX CALLS **/
+    let twitchList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+        "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"
+    ];
+
+    let len = twitchList.length;
+    for (let i = 0; i < len; i++) {
+        let url = 'https://wind-bow.gomix.me/twitch-api/streams/' + twitchList[i] + "?";
+        $.ajax({
+            url: url,
+            data: "json",
+            success: function(data) {
+                if (data.stream) {
+                    let container = document.getElementById("container");
+                    let appendString = "<div class='user-wrapper'>" +
+                        "<img src='" + data.stream.channel.logo + "' class = 'user-image'>" +
+                        "<p class='user-name'>" + data.stream.channel.display_name + "</p>" +
+                        "<span class='indicator-on'></span>" +
+                        "<p class='title'>" + data.stream.channel.game + "</p></div>";
+                    container.innerHTML += appendString;
+                } else {
+                    let channelLink = data._links.channel;
+                    let url = channelLink = channelLink.replace("https://api.twitch.tv/kraken/",
+                        "https://wind-bow.gomix.me/twitch-api/");
+                    url += "?";
+                    $.ajax({
+                        url: url,
+                        data: "json",
+                        success: function(data) {
+                            let container = document.getElementById("container");
+                            let appendString = "<div class='user-wrapper'>" +
+                                "<img src='" + data.logo + "' class = 'user-image'>" +
+                                "<p class='user-name'>" + data.display_name + "</p>" +
+                                "<span class='indicator-off'></span>" +
+                                "<p class='title'></p></div>";
+                            container.innerHTML += appendString;
+                        },
+                        error: function(err) {
+                            console.log(err);
+                        }
+                    });
+                    // end of 2nd ajax call
+                }
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+        // end of 1st ajax call
+    }
+    // end of for loop
+
+
     /** Event Listeners **/
     let allTab = $("#all")
     allTab.addClass("active");
@@ -68,57 +122,6 @@ $(document).ready(function() {
         };
     });
 
-    /** AJAX CALLS **/
-    let twitchList = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
-        "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"
-    ];
-
-    let len = twitchList.length;
-    for (let i = 0; i < len; i++) {
-        let url = 'https://wind-bow.gomix.me/twitch-api/streams/' + twitchList[i] + "?";
-        $.ajax({
-            url: url,
-            data: "json",
-            success: function(data) {
-                if (data.stream) {
-                    let container = document.getElementById("container");
-                    let appendString = "<div class='user-wrapper'>" +
-                        "<img src='" + data.stream.channel.logo + "' class = 'user-image'>" +
-                        "<p class='user-name'>" + data.stream.channel.display_name + "</p>" +
-                        "<span class='indicator-on'></span>" +
-                        "<p class='title'>" + data.stream.channel.game + "</p></div>";
-                    container.innerHTML += appendString;
-                } else {
-                    let channelLink = data._links.channel;
-                    let url = channelLink = channelLink.replace("https://api.twitch.tv/kraken/",
-                        "https://wind-bow.gomix.me/twitch-api/");
-                    url += "?";
-                    $.ajax({
-                        url: url,
-                        data: "json",
-                        success: function(data) {
-                            let container = document.getElementById("container");
-                            let appendString = "<div class='user-wrapper'>" +
-                                "<img src='" + data.logo + "' class = 'user-image'>" +
-                                "<p class='user-name'>" + data.display_name + "</p>" +
-                                "<span class='indicator-off'></span>" +
-                                "<p class='title'></p></div>";
-                            container.innerHTML += appendString;
-                        },
-                        error: function(err) {
-                            console.log(err);
-                        }
-                    });
-                    // end of 2nd ajax call
-                }
-            },
-            error: function(err) {
-                console.log(err);
-            },
-        });
-        // end of 1st ajax call
-    }
-    // end of for loop
 
 });
 // end of document ready
